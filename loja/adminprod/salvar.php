a0001bd047a6c524ec704590607f6bd2dc93eaf4
ramalho06 <150079153+ramalho06@users.noreply.github.com>
2023-11-13 15:47:45 -0300

    Add files via upload

---
+++ loja/adminprod/salvar.php
@@ -0,0 +1,20 @@
+<form action="?pg=salvardb" method="post">
+<table> 
+    <tr> 
+        <td>Nome do produto: </td>
+        <td><input name="produto" type="text"/></td>
+    </tr>
+    <tr> 
+        <td>Preço no Pix: </td>
+        <td><input name="precopix" type="text"/></td>
+    </tr>
+    <tr>
+        <td>Preço parcelado: </td>
+        <td><input name="precoparcelado" type="text"></textarea></td>
+    </tr>
+    <tr>
+        <td></td>
+        <td><button name="Enviar">Cadastrar</button></td>
+    </tr>
+</table>
+</form>
