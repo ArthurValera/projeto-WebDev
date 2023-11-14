8655dd2718b097b1ef3d589f58f5484a68677410
ArthurValera <79552236+ArthurValera@users.noreply.github.com>
2023-11-14 08:58:08 -0300

    Delete config.php

--- config.php
+++
@@ -1,9 +0,0 @@
-<!--TROCAR O NOME DO BD DE ACORDO COM O NOME DO BD DO PROJETO OFCIAL-->
-<?php
-    // conecta ao servidor
-    $conn = mysqli_connect("localhost","root","");
-
-    // conecta ao BD criado
-    $db = mysqli_select_db($conn, "clientes");
-        
-?>
