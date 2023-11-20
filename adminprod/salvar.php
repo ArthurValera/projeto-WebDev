d87e0720b9786ae52f9a5c56336dec52440449f1
ArthurValera <arthurvalera8@gmail.com>
2023-11-20 20:39:54 -0300

    Adição do login e adminprod

--- adminprod/salvar.php
+++ adminprod/salvar.php
@@ -7,23 +7,19 @@
 </head>
 <body>
   
-<form action="?pg=salvardb" enctype="multipart/form-data" method="post">
+<form action="?pg=salvardb"  method="post">
 <table> 
     <tr> 
         <td>Nome do produto: </td>
         <td><input name="produto" type="text"/></td>
     </tr>
     <tr> 
-        <td>Imagem do produto: </td>
-        <td><input name="imagem" type="file"/></td>
-    </tr>
-    <tr> 
         <td>Preço: </td>
         <td><input name="preco" type="text"/></td>
     </tr>
     <tr>
-        <td>Estoque: </td>
-        <td><input name="estoque" type="text"></textarea></td>
+        <td>Quantidade: </td>
+        <td><input name="quantidade" type="text"></textarea></td>
     </tr>
     <tr>
         <td></td>
--- adminprod/salvardb.php
+++ adminprod/salvardb.php
@@ -1,42 +1,16 @@
 <?php include "config.inc.php"; ?>
 <?php
 
-if(isset($_FILES['imagem'])){
-    $imagem=$_FILES['imagem'];
-
-    if($imagem['error'])
-        die("falha no envio do arquivo");
-
-    if($imagem['size']>5242880)
-        die("Arquivo extrapolou o limite. Máximo:5MB");
-
-    $pasta="imagens/";
-    $nomedaimagem=$imagem['name'];
-    $novoNomeDaImagem=uniqid();
-    $extensao= strtolower(pathinfo($nomedaimagem,PATHINFO_EXTENSION));  
-
-    if($extensao != 'jpg' && $extensao !='png' && $extensao != 'jpeg') 
-        die("tipo de arquivo não permitido");
-    $path=  $pasta . $novoNomeDaImagem . "." . $extensao;
-    $deu_certo = move_uploaded_file($imagem["tmp_name"],$path);
-
-    //if($deu_certo){
-       // $sql = "INSERT INTO produtos(nome,path,) VALUES('$nomedaimagem', '$path')";
-        
-    //}else
-    //echo"falha ao enviar o arquivo";
-    
-}
-
 $produto = $_POST['produto'];
 $preco = $_POST['preco'];
-$estoque = $_POST['estoque'];
-if($deu_certo){
+$quantidade = $_POST['quantidade'];
 
-$sql = "INSERT INTO produtos(produto,preco,estoque,nome,path) VALUES 
-( '$produto', '$preco', '$estoque','$nomedaimagem','$path')";
-}
-$insert = mysqli_query($conexao, $sql);
+
+$sql = "INSERT INTO produtos(produto,preco,quantidade) VALUES 
+( '$produto', '$preco', '$quantidade')";
+
+$insert = mysqli_query($conn, $sql);
+
 echo @$sql;
 
 if(!$insert){
